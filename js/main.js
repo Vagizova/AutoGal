c22bdfe87d7d5edd912f5cd4fcb19bd93a519c57
Vagizova <vagizova.rita@gmail.com>
2017-02-28 17:38:22 +0500

    autoGal

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+$(document).on('ready', function() {
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        prevArrow: arrowleft,
+	        nextArrow: arrowright
+       
+        
+      });
+       });
+// $('.var-width').slick({
+//   infinite: true,
+//   speed: 250,
+// // определяем скорость перелистывания
+ 
+//   slidesToShow: 1,
+// //количество слайдов для показа
+//   centerMode: true,
+// //текущий слайд по центру
+//   variableWidth: true
+// //установим переменную ширину
+// });
