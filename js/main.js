68cf29cd337fb2a4edc2923daa54812d81b688a6
Vagizova <vagizova.rita@gmail.com>
2017-03-01 11:01:48 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -10,7 +10,16 @@ $(document).on('ready', function() {
        
         
       });
+
+      $("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 60;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
        });
+
+
+
 // $('.var-width').slick({
 //   infinite: true,
 //   speed: 250,
